cc0ad59486e93e9409656e0f8d80a4634e436c0a
Sharlene Cera <cera.sharlene@gmail.com>
2024-11-19 20:40:18 +0000

    Wrote the template code for RegularItems.py

---
+++ RegularItems.py
@@ -0,0 +1,26 @@
+from BaseInventoryItem import InventoryItem
+
+class RegularItem(InventoryItem):
+    def add_stock(self, amount):
+        self.quantity += amount
+
+    def remove_stock(self, amount):
+        if amount > self.quantity:
+            raise ValueError("Not enough stock")
+        self.quantity -= amount
+
+class PerishableItem(InventoryItem):
+    def __init__(self, name, quantity, expiry_date):
+        super().__innit__(name, quantity)
+        self.expiry_date = expiry_date
+    
+    def add_stock(self, amount):
+        self.quantity += amount
+
+    def remove_stock(self, amount):
+        if amount > self.quantity:
+            raise ValueError("Not enough stock")
+        self.quantity -= amount
+
+    def __str__(self):
+        return f"{self.name} (Expires: {self.expiry_date}): {self.quantity}"
