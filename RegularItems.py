aaa2bcd15e3e4cd5a32988440d9d7b98ab470821
Sharlene Cera <cera.sharlene@gmail.com>
2024-12-02 14:46:55 +0000

    Fixed __innit__ misspelling, deleted duplicate code by making PerishableItem inherit from RegularItem.

--- RegularItems.py
+++ RegularItems.py
@@ -9,18 +9,10 @@ class RegularItem(InventoryItem):
             raise ValueError("Not enough stock")
         self.quantity -= amount
 
-class PerishableItem(InventoryItem):
+class PerishableItem(RegularItem):
     def __init__(self, name, quantity, expiry_date):
-        super().__innit__(name, quantity)
+        super().__init__(name, quantity)
         self.expiry_date = expiry_date
-    
-    def add_stock(self, amount):
-        self.quantity += amount
-
-    def remove_stock(self, amount):
-        if amount > self.quantity:
-            raise ValueError("Not enough stock")
-        self.quantity -= amount
 
     def __str__(self):
         return f"{self.name} (Expires: {self.expiry_date}): {self.quantity}"
