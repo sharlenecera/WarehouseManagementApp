462bc0e2701b5a908dc5f879128a16b2d75f75b4
Sharlene Cera <cera.sharlene@gmail.com>
2024-12-03 14:26:24 +0000

    Added property decorators to achieve getter and setter behaviour

--- BaseInventoryItem.py
+++ BaseInventoryItem.py
@@ -1,9 +1,22 @@
 # Abstraction: Define abstract class for a generic inventory item
 class InventoryItem:
     def __init__(self, name, quantity):
-        self.name = name
-        self.quantity = quantity
+        self._name = name
+        self._quantity = quantity
 
+    @property
+    def name(self):
+        return self._name
+
+    # if we add ability to change the name
+    # @name.setter
+    # def name(self, new_name):
+    #     self._name = new_name
+
+    @property
+    def quantity(self):
+        return self._quantity
+    
     def add_stock(self, amount):
         raise NotImplementedError
     
@@ -11,4 +24,4 @@ class InventoryItem:
         raise NotImplementedError
     
     def __str__(self):
-        return f"{self.name}: {self.quantity}"
+        return f"{self._name}: {self._quantity}"
--- RegularItems.py
+++ RegularItems.py
@@ -2,17 +2,21 @@ from BaseInventoryItem import InventoryItem
 
 class RegularItem(InventoryItem):
     def add_stock(self, amount):
-        self.quantity += amount
+        self._quantity += amount
 
     def remove_stock(self, amount):
         if amount > self.quantity:
             raise ValueError("Not enough stock")
-        self.quantity -= amount
+        self._quantity -= amount
 
 class PerishableItem(RegularItem):
     def __init__(self, name, quantity, expiry_date):
         super().__init__(name, quantity)
-        self.expiry_date = expiry_date
+        self._expiry_date = expiry_date
+
+    @property
+    def expiry_date(self):
+        return self._expiry_date
 
     def __str__(self):
         return f"{self.name} (Expires: {self.expiry_date}): {self.quantity}"
